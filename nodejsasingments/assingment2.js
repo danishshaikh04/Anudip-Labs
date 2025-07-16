ce6cf147dda1a645b2221a57e511f9bf30e4021f
danishshaikh04 <danishshaikh0595@gmail.com>
2025-07-16 16:48:40 +0530

    assingment 2  print the characters in string

---
+++ nodejsasingments/assingment2.js
@@ -0,0 +1,7 @@
+function len(str1, str2, str3){
+    console.log(`number of character in string 1: ${str1.length}`);
+    console.log(`number of character in string 2: ${str2.length}`);
+    console.log(`number of character in string 3: ${str3.length}`);
+}
+
+len("hello", "world", "My name is Mohammed Danish Shaikh")

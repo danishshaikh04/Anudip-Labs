43515e4e75a3eba6f61a96aeba6b527c809eb891
danishshaikh04 <danishshaikh0595@gmail.com>
2025-07-16 16:45:52 +0530

    assingment1 merge two string

---
+++ nodejsasingments/assingment1.js
@@ -0,0 +1,9 @@
+function concat(){
+    let str1 = "Hello, this is my first Node.js program!";
+    let str2 = "I am excited to learn more about Node.js!";
+
+    console.log(str1);
+    console.log(str2);
+    console.log(`${str1} and ${str2}`);
+}
+concat();
